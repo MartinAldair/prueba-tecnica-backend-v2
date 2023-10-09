59eb8cc1c27a90b8840d6657f2471ff6582a07c2
Martin Aldair <martin.aldair.ramirez.ramos@outlook.com>
2023-10-09 15:36:28 -0600

    Upload resolucion ejercicio 2

--- src/test/java/com/martinaldair/backend/examenv2/Examenv2ApplicationTests.java
+++ src/test/java/com/martinaldair/backend/examenv2/Examenv2ApplicationTests.java
@@ -8,9 +8,38 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.martinaldair.backend.examenv2.util.MiExcepcionPersonalizada;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
 class Examenv2ApplicationTests {
+    
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    /**
+     * Ejercicio 2
+     * 
+     * Dado la siguiente clase agrega el manejo de errores correspondientes y justifica él porque
+     */
+    @Test
+    void test_Ejercicio_2() {
+        System.out.println("Intentamos ejecutar el bloque de instrucciones:");
+        System.out.println("Instrucción 1.");
+        /* 
+        En el presente ejercicio se utiliza el bloque try-catch, porque sirve para
+        saber el tipo de excepción que esta en el metodo parseInt() de la clase Integer.
+        Al momento ejecutarse el metodo parseInt() y al no encontrar caracteres de
+        tipo numerico y solamente letras, se produce una excepción de tipo 
+        java.lang.NunmberFormatException
+         */
+        try {
+            int n = Integer.parseInt("M"); //error forzado en tiempo de ejecución.
+        } catch (Exception ex) {
+            log.error("Tipo de error: " + ex);
+        }
+        System.out.println("Instrucción 2.");
+        System.out.println("Instrucción 3, etc.");
+    }
     
     /**
      * Ejercicio 3
