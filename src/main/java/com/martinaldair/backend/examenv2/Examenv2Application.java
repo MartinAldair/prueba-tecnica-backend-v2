0235beed894948930989d7057e37bcad9402f28e
Martin Aldair <martin.aldair.ramirez.ramos@outlook.com>
2023-10-09 14:53:47 -0600

    first commit

---
+++ src/main/java/com/martinaldair/backend/examenv2/Examenv2Application.java
@@ -0,0 +1,13 @@
+package com.martinaldair.backend.examenv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examenv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examenv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/martinaldair/backend/examenv2/Examenv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinaldair.backend.examenv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examenv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
