3d5970549c1e2470026e0b117a03a0746e110bc9
Martin Aldair <martin.aldair.ramirez.ramos@outlook.com>
2023-10-09 18:01:23 -0600

    Upload integracion open api 3

--- src/main/java/com/martinaldair/backend/examenv2/Examenv2Application.java
+++ src/main/java/com/martinaldair/backend/examenv2/Examenv2Application.java
@@ -1,13 +1,48 @@
 package com.martinaldair.backend.examenv2;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import io.swagger.configuration.LocalDateConverter;
+import io.swagger.configuration.LocalDateTimeConverter;
 
 @SpringBootApplication
-public class Examenv2Application {
+@ComponentScan(basePackages = { "com.martinaldair", "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+public class Examenv2Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Examenv2Application.class, args);
-	}
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
 
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(Examenv2Application.class).run(args);
+    }
+
+    @Configuration
+    static class CustomDateConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+            registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+        }
+    }
+
+    class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
 }
